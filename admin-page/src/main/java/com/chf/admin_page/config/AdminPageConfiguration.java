acf25094e1020663f1c6246b25f79ee4a9e351de
chf <hi.phone.chan@gmail.com>
2022-08-23 21:42:57 +0800

    page config

--- admin-page/src/main/java/com/chf/admin_page/config/AdminPageConfiguration.java
+++ admin-page/src/main/java/com/chf/admin_page/config/AdminPageConfiguration.java
@@ -1,18 +1,18 @@
 package com.chf.admin_page.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
+@ComponentScan("com.chf.admin_page")
 @Configuration
 public class AdminPageConfiguration implements WebMvcConfigurer {
-    
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-      registry.addResourceHandler("/page/admin/**").addResourceLocations("classpath:/META-INF/resources/webjars/admin/");
-      registry.addResourceHandler("/page/admin").addResourceLocations("classpath:/META-INF/resources/webjars/admin/index.html");
+        registry.addResourceHandler("/page/admin/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/admin/");
     }
 
 }
---
+++ admin-page/src/main/java/com/chf/admin_page/web/rest/AdminClientForwardController.java
@@ -0,0 +1,22 @@
+package com.chf.admin_page.web.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminClientForwardController {
+
+    /**
+     * Forwards any unmapped paths (except those containing a period) to the client {@code index.html}.
+     * @return forward to client {@code index.html}.
+     */
+    @GetMapping(value = "/page/admin/**/{path:[^\\.]*}")
+    public String otherForward() {
+        return "forward:/page/admin/index.html";
+    }
+    
+    @GetMapping(value = "/page/admin")
+    public String defaultForward() {
+        return "forward:/page/admin/index.html";
+    }
+}
